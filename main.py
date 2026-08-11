a420cde0ae1921ebf3e356f6bab9ef248abcab5c
PaciStardust <git@paci.dev>
2026-08-11 22:43:11 +0200

    feat: :sparkles: loading of photographer data

--- main.py
+++ main.py
@@ -213,7 +213,7 @@ class CsvData:
                 self.lookup_shortcut_camera.append(camera)
 
 class PictureData:
-    def __init__(self, path: str, focal_length: float, focal_length_in_35mm_format: int, iso: int, exposure_time: float, aperture: float, created: datetime.datetime, cam: CameraData, lens: LensData):
+    def __init__(self, path: str, focal_length: float, focal_length_in_35mm_format: int, iso: int, exposure_time: float, aperture: float, created: datetime.datetime, photographer: str, cam: CameraData, lens: LensData):
         self.path = path
         self.focal_length = focal_length
         self.focal_length_in_35mm_format = focal_length_in_35mm_format
@@ -223,6 +223,7 @@ class PictureData:
         self.camera = cam
         self.lens = lens
         self.created = created
+        self.photographer = photographer
 
     def name(self) -> str:
         txt_focal = '?' if self.focal_length == 0 else self.focal_length
@@ -252,6 +253,8 @@ class PictureData:
             diffs[TAG_PIC_EXPOSURE] = (str(self.exposure_time), str(other.exposure_time))
         if not float_compare(self.aperture, other.aperture):
             diffs[TAG_PIC_APERTURE] = (str(self.aperture), str(other.aperture))
+        if self.photographer != other.photographer:
+            diffs[TAG_PIC_PHOTOGRAPHER] = (self.photographer, other.photographer)
         return diffs
 
 NodePics = Dict[str,PictureData]
@@ -355,7 +358,8 @@ def perform_scan(directory: str, exif: exiftool.ExifToolHelper) -> Dict[str,List
         TAG_LENS_MODEL,
         TAG_PIC_FOCAL,
         TAG_PIC_FOCAL35,
-        TAG_PIC_DATE_TIME_ORIGINAL
+        TAG_PIC_DATE_TIME_ORIGINAL,
+        TAG_PIC_PHOTOGRAPHER
     ]
 
     for (i_dir,(dir,files)) in enumerate(dir_files.items()):
@@ -428,8 +432,9 @@ def convert_scanned_data(dataDict: dict) -> PictureData:
     pic_focal = float_or_zero(dataDict[TAG_PIC_FOCAL]) if TAG_PIC_FOCAL in dataDict else 0.0
     pic_exposure = float_or_zero(dataDict[TAG_PIC_EXPOSURE]) if TAG_PIC_EXPOSURE in dataDict else 0.0
     pic_date = datetime_or_min(dataDict[TAG_PIC_DATE_TIME_ORIGINAL] if TAG_PIC_DATE_TIME_ORIGINAL in dataDict else '')
+    pic_photographer = dataDict[TAG_PIC_PHOTOGRAPHER] if TAG_PIC_PHOTOGRAPHER in dataDict else ''
 
-    return PictureData(path, pic_focal, pic_focal35, pic_iso, pic_exposure, pic_aperture, pic_date, cam, lens)
+    return PictureData(path, pic_focal, pic_focal35, pic_iso, pic_exposure, pic_aperture, pic_date, pic_photographer, cam, lens)
 
 regex_pixel_file_extractor = re.compile('^PXL_[0-9]+_[0-9]+')
 def fix_naming_system_irregularities(pic: PictureData, name: str) -> str:
@@ -911,8 +916,9 @@ COL_PIC_ISO = 'Pic_Iso'
 COL_PIC_EXPO = 'Pic_Expo'
 COL_PIC_APER = 'Pic_Aper'
 COL_PIC_CREAT = 'Pic_Creat'
+COL_PIC_PHOTO = 'Pic_Photo'
 
-COLS_STR: List[str] = [COL_FILE_FOLDER, COL_CAM_MAKE, COL_CAM_MODEL, COL_LENS_MODEL]
+COLS_STR: List[str] = [COL_FILE_FOLDER, COL_CAM_MAKE, COL_CAM_MODEL, COL_LENS_MODEL, COL_PIC_PHOTO]
 COLS_FLOAT: List[str] = [COL_LENS_FOCAL_MIN, COL_LENS_FOCAL_MAX, COL_LENS_APER_MIN, COL_LENS_APER_MAX, COL_PIC_FOCAL, COL_PIC_EXPO, COL_PIC_APER]
 COLS_INT: List[str] = [COL_PIC_FOCAL35, COL_PIC_ISO]
 COLS_DATE: List[str] = [COL_PIC_CREAT]
@@ -1021,7 +1027,8 @@ def analyze_data(node_dirs: NodeDirs):
                 COL_PIC_APER: pic.aperture,
                 COL_PIC_FOCAL35: pic.focal_length_in_35mm_format,
                 COL_PIC_ISO: pic.iso,
-                COL_PIC_CREAT: pic.created
+                COL_PIC_CREAT: pic.created,
+                COL_PIC_PHOTO: pic.photographer
             })
 
     main_data_frame = pd.DataFrame(data_dict)
